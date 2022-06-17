a098b97ff630b8542bab4dea6a728b065e1b4297
Serhiy <serhiy@Serhiys-iMac.local>
2022-06-16 22:19:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        printResult();
+        int number1=30;
+        System.out.println(number1);
+
+
+    }
+
+    public static void printResult() {
+
+        System.out.println(5*40);
+    }
+
+
+
+}
